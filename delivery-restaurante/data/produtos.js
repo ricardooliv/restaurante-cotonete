bdadf241b998ae2957d297a7c35567a5b78bddd0
ricardooliv <61585445+ricardooliv@users.noreply.github.com>
2026-03-12 20:50:18 -0300

    Add files via upload

---
+++ delivery-restaurante/data/produtos.js
@@ -0,0 +1,22 @@
+const produtos = {
+
+quentinhas: [
+{nome: "Quentinha de Carne", preco: 18},
+{nome: "Quentinha de Frango", preco: 18},
+{nome: "Quentinha de Toscana", preco: 18},
+{nome: "Quentinha Mista", preco: 20}
+],
+
+parmegiana: [
+{nome: "Filé à Parmegiana Executivo", preco: 25},
+{nome: "Filé à Parmegiana Médio", preco: 35},
+{nome: "Filé à Parmegiana Grande", preco: 45}
+],
+
+churrasco: [
+{nome: "Churrasco Maminha 500g", preco: 55},
+{nome: "Churrasco Maminha 750g", preco: 75},
+{nome: "Churrasco Maminha 1kg", preco: 95}
+]
+
+};
